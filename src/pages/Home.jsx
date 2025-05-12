d0207f675fe8e232de2ee9e69d30072e14575b87
Eyoab <eyoabamare3@gmail.com>
2025-05-12 11:24:32 -0700

    Added laptop component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,20 @@
 import HeroSection from "../Components/Home/HeroSection";
 import SnowEffect from "../Components/Home/SnowEffect";
 import NavBar from "../Components/Shared/NavBar";
-
+import Laptop from "../Components/Home/Laptop";
 
 const Home = () => {
   return (
-    <div className="min-h-screen bg-grey-50 flex flex-col ">
-      <div className="m-8 flex-1 bg-gradient-to-b from-blue-100 to-blue-200 flex flex-col relative">
+    <div className="bg-gray-50 flex flex-col">
+      {/* First section: SnowEffect, NavBar, HeroSection */}
+      <div className="min-h-screen m-8 bg-gradient-to-b from-blue-100 to-blue-200 flex flex-col relative">
         <SnowEffect />
         <NavBar />
         <HeroSection />
-
-        
+      </div>
+      {/* Second section: Laptop */}
+      <div className="m-8">
+        <Laptop />
       </div>
     </div>
   );
